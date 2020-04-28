537f31f660c858f460b04fe847e2e93bb3be7b3d
israrhussainrao <61529156+israrhussainrao@users.noreply.github.com>
2020-04-28 15:44:18 +0500

    Add files via upload
    simple python codes for practice

---
+++ learning of python/learning.py
@@ -0,0 +1,27 @@
+command = ""
+started = False
+while True:
+    command = input(">").lower()
+    if command == "start":
+        if started:
+            print("the car already started...")
+        else:
+            started = True
+            hel = True
+            print("car started")
+    elif command == "stop":
+        if not started:
+            print("the car already stopped")
+        else:
+            started = False
+            print("car stopped")
+    elif command == "help":
+        print('''
+start - car start
+stop - car stop
+quit - to quit
+        ''')
+    elif command == "quit":
+        break
+    else:
+        print("i don't understand write 'help' to understand the command")
